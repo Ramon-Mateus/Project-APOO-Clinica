05fea9f78f0130f8f17c81c152ed8e5326ec5f07
Ramon de Oliveira Araújo Mateus <ramon.mateus@academico.ifrn.edu.br>
2022-10-25 13:04:25 -0300

    Exame DAL

--- WebApplication/Controllers/ProcedimentosController.cs
+++ WebApplication/Controllers/ProcedimentosController.cs
@@ -4,47 +4,95 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebApplication.Models;
+using WebApplication.DAL.Cadastros;
+using System.Net;
 
 namespace WebApplication.Controllers
 {
     public class ProcedimentosController : Controller
     {
+        private ExameDAL exameDAL = new ExameDAL();
 
-        private static IList<Exame> exames = new List<Exame>()
+        private ActionResult ObterVisaoExamePorId(long? id)
         {
-            new Exame() { ExameId = 0, Descricao = "Próstata"},
-            new Exame() { ExameId = 1, Descricao = "Vista"},
-            new Exame() { ExameId = 2, Descricao = "Sangue"},
-            new Exame() { ExameId = 3, Descricao = "Tomografia"},
-            new Exame() { ExameId = 4, Descricao = "Ressonância"},
-        };
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(
+                HttpStatusCode.BadRequest);
+            }
+            Exame exame = exameDAL.ObterExamePorId((long)id);
+            if (exame == null)
+            {
+                return HttpNotFound();
+            }
+            return View(exame);
+        }
 
-        // GET: Exames
+        private ActionResult GravarExame(Exame exame)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    exameDAL.GravarExame(exame);
+                    return RedirectToAction("Index");
+                }
+                return View(exame);
+            }
+            catch
+            {
+                return View(exame);
+            }
+        }
+
+
         public ActionResult Index()
         {
-            return View(exames);
+            return View(exameDAL.ObterExamesClassificadosPorDescricao());
         }
-
-        // GET: Exames/Details/5
-        /*public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Exames/Create
+        
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Exames/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Exame exame)
+        {
+            return GravarExame(exame);
+        }
+        
+        public ActionResult Edit(long? id)
+        {
+            return ObterVisaoExamePorId(id);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Exame exame)
+        {
+            return GravarExame(exame);
+        }
+
+        public ActionResult Details(long? id)
+        {
+            return ObterVisaoExamePorId(id);
+        }
+
+        public ActionResult Delete(long? id)
+        {
+            return ObterVisaoExamePorId(id);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(long id)
         {
             try
             {
-                // TODO: Add insert logic here
-
+                Exame exame = exameDAL.EliminarExamePorId(id);
+                TempData["Message"] = "Exame " + exame.Descricao.ToUpper() + " foi removido";
                 return RedirectToAction("Index");
             }
             catch
@@ -52,49 +100,5 @@ namespace WebApplication.Controllers
                 return View();
             }
         }
-
-        // GET: Exames/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Exames/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Exames/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Exames/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }*/
     }
 }
